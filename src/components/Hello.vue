d68984511f608032185ad41384d6022b672b18e4
wangxinoo <wangxin_oo@hotmail.com>
2018-06-06 11:36:36 +0800

    Initial commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,18 @@
+<template>
+    <p>
+    123
+    {{msg}}
+    <a href="#/hello">转到首页</a>
+    </p>
+</template>
+
+<script>
+export default {
+name: 'HelloWorld',
+data () {
+  return {
+    msg: 'Welcome to Your Vue.js App12311112343'
+  }
+}
+}
+</script>
